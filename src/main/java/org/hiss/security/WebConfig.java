fa02ea6dda1c11e5f7c96cf5fad2a91a3a87407f
Joseph Rydel <jrydel92@gmail.com>
2025-03-13 15:53:01 -0500

    Made small changes.

--- src/main/java/org/hiss/security/WebConfig.java
+++ src/main/java/org/hiss/security/WebConfig.java
@@ -5,36 +5,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-//@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
-
-//    @Bean
-//    public FilterRegistrationBean corsFilter() {
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowCredentials(true);
-//        configuration.addAllowedOrigin("http://localhost:4200/");
-//        configuration.setAllowedHeaders(Arrays.asList(
-//                HttpHeaders.AUTHORIZATION,
-//                HttpHeaders.CONTENT_TYPE,
-//                HttpHeaders.ACCEPT
-//        ));
-//
-//        configuration.setAllowedMethods(Arrays.asList(
-//                HttpMethod.GET.name(),
-//                HttpMethod.POST.name(),
-//                HttpMethod.PUT.name(),
-//                HttpMethod.PATCH.name(),
-//                HttpMethod.DELETE.name()
-//        ));
-//
-//        configuration.setMaxAge(3600L);
-//        source.registerCorsConfiguration("/**", configuration);
-//        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-//        bean.setOrder(-102); //Set to lowest position to execute before any Spring bean.
-//
-//        return bean;
-//    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -44,7 +15,4 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
-
-
-
 }
